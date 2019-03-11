40194d705470a1a6c1fc48fa1bfc9e1c51d33b1e
tam84 <thiago_martinho@outlook.com>
2019-03-11 18:31:41 -0300

    remove yarn

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -64,5 +64,5 @@
 #set :stage, :production
 
 #server 'ec2-18-191-49-166.us-east-2.compute.amazonaws.com', user: 'deploy', roles: %w{web app db}
-server '18.216.213.127.us-east-2.compute.amazonaws.com', user: 'deploy', roles: %w{web app db}
+server 'ec2-18-217-155-28.us-east-2.compute.amazonaws.com', user: 'deploy', roles: %w{web app db}
 
