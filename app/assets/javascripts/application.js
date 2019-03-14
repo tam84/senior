bc6d4a1130301a6204f136035bce11d4b8068c5f
tam84 <thiago_martinho@outlook.com>
2019-03-14 10:30:00 -0300

    teste

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery2
-//= require rails-ujs
+//= require jquery_ujs
 //= require popper
 //= require material/core/bootstrap-material-design.min.js
 //= require material/plugins/perfect-scrollbar.jquery.min.js
