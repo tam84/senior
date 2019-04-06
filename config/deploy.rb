484b96c6c924e0ff62568307e71b645966587efe
tam84 <thiago_martinho@outlook.com>
2019-04-06 12:31:03 -0300

    t

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url, 'git@github.com:tam84/fertilizacao.git' # Edit this to match your
 set :branch, :master
 set :deploy_to, '/home/deploy/tam84_v2'
 set :pty, true
-set :linked_files, %w(config/database.yml config/secrets.yml)
+set :linked_files, %w(config/database.yml config/secrets.yml config/master.key)
 set :linked_dirs, %w(bin log tmp)
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
