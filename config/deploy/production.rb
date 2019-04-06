514c400554ac77a1dc185e01d55fa297c0e7fcb9
tam84 <thiago_martinho@outlook.com>
2019-04-06 09:31:29 -0300

    t

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -64,4 +64,4 @@
 #set :stage, :production
 
 #server 'ec2-18-191-49-166.us-east-2.compute.amazonaws.com', user: 'deploy', roles: %w{web app db}
-server 'ec2-18-225-31-211.us-east-2.compute.amazonaws.com', user: 'deploy', roles: %w{web app db}
+server 'ec2-18-223-241-60.us-east-2.compute.amazonaws.com', user: 'deploy', roles: %w{web app db}
